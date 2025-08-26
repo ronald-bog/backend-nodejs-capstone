5ff2403266c04a0cbaf05e812c439d3214bf2f37
ronald-bog <ronald79@gmail.com>
2025-08-26 14:35:36 -0400

    Step 5 evidencia

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
+const path = require('path');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
-
 
 const app = express();
 app.use("*",cors());
@@ -27,11 +27,10 @@ app.use(express.json());
 //{{insert code here}}
 
 // Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
-//{{insert code here}}
+const secondChanceRoutes = require('./routes/secondChanceItemsRoutes');
 
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
-
+const searchRoutes = require('./routes/searchRoutes');
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -43,10 +42,10 @@ app.use(pinoHttp({ logger }));
 //{{insert code here}}
 
 // Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/secondchance/items', secondChanceRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/secondchance/search', searchRoutes);
 
 
 // Global Error Handler
--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
+require('dotenv').config();
 
 // Search for gifts
 router.get('/', async (req, res, next) => {
@@ -8,7 +9,7 @@ router.get('/', async (req, res, next) => {
         // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
         const db = await connectToDatabase();
 
-        const collection = db.collection("gifts");
+        const collection = db.collection(process.env.MONGO_COLLECTION);
 
         // Initialize the query object
         let query = {};
