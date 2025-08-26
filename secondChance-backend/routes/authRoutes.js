29755c303d20ab2ebc09101f30fe7740f22f2a81
ronald-bog <ronald79@gmail.com>
2025-08-26 15:19:45 -0400

    Mod 3 lab 3

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -99,4 +99,51 @@ router.post('/login', async (req, res) => {
       }
 });
 
+// update
+router.put('/update', async (req, res) => {
+    // Tarea 2: Validar la entrada usando `validationResult` y devolver un mensaje apropiado si hay un error.
+    const errors = validationResult(req);
+    // Tarea 3: Comprobar si `email` está presente en el encabezado y lanzar un mensaje de error apropiado si no está presente.
+    if (!errors.isEmpty()) {
+        logger.error('Errores de validación en la solicitud de actualización', errors.array());
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const email = req.headers.email;
+        if (!email) {
+            logger.error('Correo electrónico no encontrado en los encabezados de la solicitud');
+            return res.status(400).json({ error: "Correo electrónico no encontrado en los encabezados de la solicitud" });
+        }
+        //Tarea 4: Conectar a MongoDB
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+        //Tarea 5: Encontrar las credenciales del usuario
+        const existingUser = await collection.findOne({ email });
+        if (!existingUser) {
+            logger.error('Usuario no encontrado');
+            return res.status(404).json({ error: "Usuario no encontrado" });
+        }
+        existingUser.firstName = req.body.name;
+        existingUser.updatedAt = new Date();
+        //Tarea 6: Actualizar las credenciales del usuario en la base de datos
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: existingUser },
+            { returnDocument: 'after' }
+        );
+        //Tarea 7: Crear autenticación JWT con user._id como carga útil usando la clave secreta del archivo .env
+        const payload = {
+            user: {
+                id: updatedUser._id.toString(),
+            },
+        };
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        logger.info('Usuario actualizado con éxito');
+        res.json({ authtoken });
+    } catch (error) {
+        logger.error(error);
+        return res.status(500).send("Error interno del servidor");
+    }
+});
+
 module.exports = router;
