f13e1ba9d92dc17781654b1c1afa22fecc45a922
ronald-bog <ronald79@gmail.com>
2025-08-26 12:45:56 -0400

    step 4 y 5

--- secondChance-backend/routes/secondChanceItemsRoutes.js
+++ secondChance-backend/routes/secondChanceItemsRoutes.js
@@ -74,9 +74,16 @@ router.post('/', upload.single('file'), async(req, res,next) => {
 router.get('/:id', async (req, res, next) => {
     try {
         //Step 4: task 1 - insert code here
+        const db = await connectToDatabase();
         //Step 4: task 2 - insert code here
+        const collection = db.collection("secondChanceItems");
         //Step 4: task 3 - insert code here
+        const secondChanceItem = await collection.findOne({ id: id });
         //Step 4: task 4 - insert code here
+        if (!secondChanceItem) {
+    return res.status(404).send("secondChanceItem not found");
+}
+res.json(secondChanceItem);
     } catch (e) {
         next(e);
     }
@@ -86,10 +93,31 @@ router.get('/:id', async (req, res, next) => {
 router.put('/:id', async(req, res,next) => {
     try {
         //Step 5: task 1 - insert code here
+        const db = await connectToDatabase();
         //Step 5: task 2 - insert code here
+        const collection = db.collection("secondChanceItems");
         //Step 5: task 3 - insert code here
+        const secondChanceItem = await collection.findOne({ id });
+
+        if (!secondChanceItem) {
+        logger.error('secondChanceItem not found');
+        return res.status(404).json({ error: "secondChanceItem not found" });
+        }
         //Step 5: task 4 - insert code here
+        secondChanceItem.category = req.body.category;
+        secondChanceItem.condition = req.body.condition;
+        secondChanceItem.age_days = req.body.age_days;
+        secondChanceItem.description = req.body.description;
+        secondChanceItem.age_years = Number((secondChanceItem.age_days/365).toFixed(1));
+        secondChanceItem.updatedAt = new Date();
+
+        const updatepreloveItem = await collection.findOneAndUpdate(
+            { id },
+            { $set: secondChanceItem },
+            { returnDocument: 'after' }
+        );
         //Step 5: task 5 - insert code here
+        res.status(201).json(updatepreloveItem);
     } catch (e) {
         next(e);
     }
